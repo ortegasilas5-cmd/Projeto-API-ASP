bfb8599fbe56c84b7eebd0c5eab9fd3cd11e3ed2
Silas <ortegasilas5@gmail.com>
2026-06-17 20:16:47 -0300

    feat: criando a controller

--- APIProjeto/Controllers/HomeController.cs
+++ APIProjeto/Controllers/HomeController.cs
@@ -7,21 +7,22 @@ namespace APIProjeto.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
-        public IActionResult Index()
+        public async  Task<IActionResult> Index()
         {
+            var cliente = _httpClientFactory.CreateClient();
+            var response = await cliente.GetFromJsonAsync<List<Postagem>>("");
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
