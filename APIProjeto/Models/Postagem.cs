8c65bbdb4416124f4e16f0f1b529c63434925602
Silas <ortegasilas5@gmail.com>
2026-06-17 19:54:10 -0300

    feat: Criando a Model

---
+++ APIProjeto/Models/Postagem.cs
@@ -0,0 +1,11 @@
+﻿namespace APIProjeto.Models
+{
+    public class Postagem
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Body { get; set; }
+    }
+}
